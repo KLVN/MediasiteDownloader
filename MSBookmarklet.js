4a22bebdf99fe2f335756a3567b14116888d43b8
KLVN <KLVN@users.noreply.github.com>
2018-08-25 13:48:24 +0200

    Better implementation

--- MSBookmarklet.js
+++ MSBookmarklet.js
@@ -1,15 +1,34 @@
-javascript:(function() {
-    var page = window.location.href;
-    if(new RegExp(".+Mediasite\/Play\/.+\?usehtml5=true", "i").test(page)) {
-        var vidSource = (document.getElementById("MediaElement").children)[0].src;
-        var a = document.createElement("a");
-        a.href = vidSource;
-        a.setAttribute("download", "");
-        document.body.appendChild(a);
-        a.click();
-        a.remove();
-    } else if(new RegExp(".+Mediasite\/Play\/.+", "i").test(page)) {
-        alert("Redirecting to HTML5 website...\nClick again on Bookmarklet to download video.\n\nhttps://github.com/KLVN/MediasiteDownloader");
-        location.href = page + "?usehtml5=true";
+javascript: (function() {
+  $.ajax({
+    url:
+        'http://webcast.tu-harburg.de/Mediasite/PlayerService/PlayerService.svc/json/GetPlayerOptions',
+    type: 'POST',
+    data: JSON.stringify({
+      'getPlayerOptionsRequest': {
+        'ResourceId': document.getElementById('ResourceId').innerHTML,
+        'QueryString': '',
+        'UseScreenReader': 'false',
+        'UrlReferrer': ''
+      }
+    }),
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
+    success: function(playerOpts) {
+      var allMedia = playerOpts.d.Presentation.Streams[1].VideoUrls;
+      for (var i = 0; i < allMedia.length; i++) {
+        if (allMedia[i].MediaType == 'MP4') {
+          var mp4Url = allMedia[i].Location;
+          $('body').append(
+              '<div id="dialog"><ul><li>Copy the title:<br><input type="text" onClick="this.select();" value="' +
+              document.title + '"></li><li><a href=' + mp4Url +
+              ' target="_blank">Right click on this link and then "Save as..."</a></li><li>Paste in to rename file correctly and save it</li><li>Click <a href="https://klvn.github.io/MediasiteDownloader/" target="_blank">here<a/> for more detailed instructions</li></ul></div>');
+          $('<style type=\'text/css\'>.ui-dialog{ z-index:999;} li{padding-bottom:20px;}</style>')
+              .appendTo('head');
+          $(function() {
+            $('#dialog').dialog()
+          })
+        }
+      }
     }
-})();
+  })
+})()
