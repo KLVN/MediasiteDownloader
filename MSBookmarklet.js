8297fee3ca8115b4afaea32751e98f2ea6ffbc89
KLVN <KLVN@users.noreply.github.com>
2019-04-03 11:16:46 +0200

    Don't download livestreams

--- MSBookmarklet.js
+++ MSBookmarklet.js
@@ -13,23 +13,7 @@ javascript: (function () {
     contentType: 'application/json; charset=utf-8',
     dataType: 'json',
     success: function (playerOpts) {
-      var allMedia = playerOpts.d.Presentation.Streams[1].VideoUrls;
-      for (var i = 0; i < allMedia.length; i++) {
-        if (allMedia[i].MediaType == 'MP4') {
-          var mp4Url = allMedia[i].Location;
-          $('body').append(
-            '<div id="open-modal" class="modal-window"> \
-              <div>\
-                <a href="#" title="Close" class="modal-close">Close</a>\
-                <ul style="list-style: inside;">\
-                  <li>Copy the title: <input type="text" onClick="this.select();" value="' + document.title + '"></li>\
-                  <li><a href=' + mp4Url + ' target="_blank">Right click on this link and then "Save as..."</a></li>\
-                  <li>Paste in to rename file correctly and save it</li>\
-                  <li>Click <a href="https://klvn.github.io/MediasiteDownloader/" target="_blank">here<a/> for more detailed instructions</li>\
-                </ul>\
-              </div>\
-            </div>');
-          $('<style type=\'text/css\'>\
+      $('<style type=\'text/css\'>\
           .modal-window {position: fixed;\
             display: flex;\
             justify-content: center;\
@@ -50,7 +34,7 @@ javascript: (function () {
           .modal-window > div {\
             width: 400px;\
             position: absolute;\
-            padding: 2em;\
+            padding: 3em;\
             background: #ffffff;\
             color: #333333;\
             border-radius: 5px;\
@@ -81,10 +65,36 @@ javascript: (function () {
           margin: 10px 0;\
           }\
           </style>')
-            .appendTo('head');
-          location.href = "#open-modal";
+        .appendTo('head');
+      if (playerOpts.d.Presentation.PlayStatus != "OnDemand") {
+        $('body').append(
+          '<div id="open-modal" class="modal-window"> \
+            <div>\
+              <a href="#" title="Close" class="modal-close">Close</a>\
+              <p style="font-size: 20px;">Lecture is currently not available on-demand and therefore cannot be downloaded.<br>Try again later.</p>\
+            </div>\
+          </div>');
+      } else {
+        var allMedia = playerOpts.d.Presentation.Streams[1].VideoUrls;
+        for (var i = 0; i < allMedia.length; i++) {
+          if (allMedia[i].MediaType == 'MP4') {
+            var mp4Url = allMedia[i].Location;
+            $('body').append(
+              '<div id="open-modal" class="modal-window"> \
+              <div>\
+                <a href="#" title="Close" class="modal-close">Close</a>\
+                <ul style="list-style: inside;">\
+                  <li>Copy the title: <input type="text" onClick="this.select();" value="' + document.title + '"></li>\
+                  <li><a href=' + mp4Url + ' target="_blank">Right click on this link and then "Save as..."</a></li>\
+                  <li>Paste in to rename file correctly and save it</li>\
+                  <li>Click <a href="https://klvn.github.io/MediasiteDownloader/" target="_blank">here<a/> for more detailed instructions</li>\
+                </ul>\
+              </div>\
+            </div>');
+          }
         }
       }
+      location.href = "#open-modal";
     }
   })
 })()
