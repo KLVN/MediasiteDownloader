e897f3f4bdbc59579a2d14e33ea1ee50feb3a347
KLVN <8047980+KLVN@users.noreply.github.com>
2022-08-22 23:05:34 +0200

    CD pipeline for minification

---
+++ minify_bookmarklet.py
@@ -0,0 +1,26 @@
+import re
+import requests
+import sys
+
+with open('MSBookmarklet.js') as input:
+  print('* Opened MSBookmarklet.js')
+  try:
+    print('* Try removing outer code that is necessary for Bookmarklets but cannot be minified by services.')
+    stripped = re.search('(?s)(?<=javascript: \(function \(\) {).*(?=}\)\(\))',  input.read()).group(0)
+  except AttributeError:
+    sys.exit('X: Could not remove outer Bookmarklet code.')
+
+  print('* POST request to minify inner code.')
+  req = requests.post('https://www.toptal.com/developers/javascript-minifier/api/raw', data={'input': stripped})
+  if req.status_code == 200:
+    print('* Online minification was successful.')
+    minified_w_spaces = req.text
+    print('* There are still unnecessary spaces inside the minified code. Will be removed now...')
+    minified = re.sub('\s{2,}', '', minified_w_spaces)
+    with open ('MSBookmarklet.min.js', 'w') as output:
+      print('* Spaces removed. Now, inserting outer code again and saving to file.')
+      output.writelines('javascript:(function(){{{}}})()'.format(minified))
+      print('* Minification done.')
+
+  else:
+    sys.exit('X: Error code from JS minifier API.')
